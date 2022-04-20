d223eef165272e4824e6ee23a20b98a422208d19
Paserno <paserno.x@gmail.com>
2022-04-20 19:39:42 -0400

    Marker

---
+++ src/components/Map.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import { View } from 'react-native';
+
+
+export const Map = () => {
+  return (
+    <View style={{ flex: 1}}>
+        <MapView
+            style={{ flex: 1}}
+            provider={ PROVIDER_GOOGLE }
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+        >
+            {/* <Marker
+              image={ require('../assets/custom-marker.png')}
+              coordinate={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+              }}
+              title='Esto es un titulo'
+              description=' Esto es una descripción del marcador'
+            /> */}
+        </MapView>
+    </View>
+  )
+}
