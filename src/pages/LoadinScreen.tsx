e8e1618b787f86bfa03764b08fb348e44aea43a3
Paserno <paserno.x@gmail.com>
2022-04-22 16:30:07 -0400

    Diseño Login

--- src/pages/LoadinScreen.tsx
+++ src/pages/LoadinScreen.tsx
@@ -1,10 +1,77 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, Platform } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { Background } from '../components/Background';
+import { WhiteLogo } from '../components/WhiteLogo';
+import { loginStyles } from '../theme/loginTheme';
 
 export const LoadinScreen = () => {
   return (
-    <View>
-        <Text>LoadinScreen</Text>
-    </View>
+    <>
+        <Background />
+
+        <View style={ loginStyles.formContainer}>
+            <WhiteLogo />
+
+            <Text style={ loginStyles.title }>Login</Text>
+
+            {/* Input Email */}
+            <Text style={ loginStyles.label }>Email:</Text>
+            <TextInput
+                placeholder='Ingrese su email:'
+                placeholderTextColor="rgba(255,255,255,0.4)"
+                keyboardType='email-address'
+                underlineColorAndroid="white"
+                style={[ 
+                    loginStyles.inputField,
+                    ( Platform.OS === 'ios') && loginStyles.inputFieldIOS
+                ]}
+                selectionColor="white"
+
+                //TODO: onchange, value
+
+                autoCapitalize='none'
+                autoCorrect={ false }
+            />
+
+            {/* Input Password */}
+            <Text style={ loginStyles.label }>Contraseña:</Text>
+            <TextInput
+                placeholder='******'
+                placeholderTextColor="rgba(255,255,255,0.4)"
+                underlineColorAndroid="white"
+                style={[ 
+                    loginStyles.inputField,
+                    ( Platform.OS === 'ios') && loginStyles.inputFieldIOS
+                ]}
+                selectionColor="white"
+
+                //TODO: onchange, value
+
+                autoCapitalize='none'
+                autoCorrect={ false }
+            />
+
+            {/* Boton Login */}
+            <View style={ loginStyles.buttonContainer}>
+                <TouchableOpacity
+                    activeOpacity={ 0.8 }
+                    style={ loginStyles.button }
+                >
+                    <Text style={ loginStyles.buttonText}> Login </Text>
+                </TouchableOpacity>
+            </View>
+
+            {/* Crear una nueva cuenta */}
+            <View style={ loginStyles.newUserContainer}>
+                <TouchableOpacity
+                    activeOpacity={ 0.8 }
+                    onPress={ () => console.log('press')}
+                >
+                    <Text style={ loginStyles.buttonText}> Nueva Cuenta </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    </>
   )
 }
