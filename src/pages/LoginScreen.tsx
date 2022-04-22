faf742ff559676a6632ce71c7a13c42232b69adb
Paserno <paserno.x@gmail.com>
2022-04-22 19:16:52 -0400

    Formulario y navegacion

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,77 +1,105 @@
 import React from 'react'
-import { Text, View, Platform } from 'react-native';
+import { Text, View, Platform, KeyboardAvoidingView, Keyboard } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import { Background } from '../components/Background';
 import { WhiteLogo } from '../components/WhiteLogo';
 import { loginStyles } from '../theme/loginTheme';
+import { useForm } from '../hooks/useForm';
+import { StackScreenProps } from '@react-navigation/stack';
 
-export const LoginScreen = () => {
+interface Props extends StackScreenProps<any, any> {}
+
+
+export const LoginScreen = ({ navigation }: Props) => {
+
+    const { email, password, onChange } = useForm({
+        email: '',
+        password: ''
+    });
+
+    const onLogin = () => {
+        console.log({email, password});
+        Keyboard.dismiss();
+    }
+
   return (
     <>
         <Background />
 
-        <View style={ loginStyles.formContainer}>
-            <WhiteLogo />
+    <KeyboardAvoidingView
+        style={{ flex: 1}}
+        behavior={ (Platform.OS === 'ios') ? 'padding' : 'height' }
+    >
 
-            <Text style={ loginStyles.title }>Login</Text>
+            <View style={ loginStyles.formContainer}>
+                <WhiteLogo />
 
-            {/* Input Email */}
-            <Text style={ loginStyles.label }>Email:</Text>
-            <TextInput
-                placeholder='Ingrese su email:'
-                placeholderTextColor="rgba(255,255,255,0.4)"
-                keyboardType='email-address'
-                underlineColorAndroid="white"
-                style={[ 
-                    loginStyles.inputField,
-                    ( Platform.OS === 'ios') && loginStyles.inputFieldIOS
-                ]}
-                selectionColor="white"
+                <Text style={ loginStyles.title }>Login</Text>
 
-                //TODO: onchange, value
+                {/* Input Email */}
+                <Text style={ loginStyles.label }>Email:</Text>
+                <TextInput
+                    placeholder='Ingrese su email:'
+                    placeholderTextColor="rgba(255,255,255,0.4)"
+                    keyboardType='email-address'
+                    underlineColorAndroid="white"
+                    style={[ 
+                        loginStyles.inputField,
+                        ( Platform.OS === 'ios') && loginStyles.inputFieldIOS
+                    ]}
+                    selectionColor="white"
 
-                autoCapitalize='none'
-                autoCorrect={ false }
-            />
+                    onChangeText={ (value) => onChange(value, 'email')}
+                    value={ email }
+                    onSubmitEditing={ onLogin }
 
-            {/* Input Password */}
-            <Text style={ loginStyles.label }>Contraseña:</Text>
-            <TextInput
-                placeholder='******'
-                placeholderTextColor="rgba(255,255,255,0.4)"
-                underlineColorAndroid="white"
-                style={[ 
-                    loginStyles.inputField,
-                    ( Platform.OS === 'ios') && loginStyles.inputFieldIOS
-                ]}
-                selectionColor="white"
+                    autoCapitalize='none'
+                    autoCorrect={ false }
+                />
 
-                //TODO: onchange, value
+                {/* Input Password */}
+                <Text style={ loginStyles.label }>Contraseña:</Text>
+                <TextInput
+                    placeholder='******'
+                    placeholderTextColor="rgba(255,255,255,0.4)"
+                    secureTextEntry={ true }
+                    underlineColorAndroid="white"
+                    style={[ 
+                        loginStyles.inputField,
+                        ( Platform.OS === 'ios') && loginStyles.inputFieldIOS
+                    ]}
+                    selectionColor="white"
 
-                autoCapitalize='none'
-                autoCorrect={ false }
-            />
+                    onChangeText={ (value) => onChange(value, 'password')}
+                    value={ password }
+                    onSubmitEditing={ onLogin }
 
-            {/* Boton Login */}
-            <View style={ loginStyles.buttonContainer}>
-                <TouchableOpacity
-                    activeOpacity={ 0.8 }
-                    style={ loginStyles.button }
-                >
-                    <Text style={ loginStyles.buttonText}> Login </Text>
-                </TouchableOpacity>
+                    autoCapitalize='none'
+                    autoCorrect={ false }
+                />
+
+                {/* Boton Login */}
+                <View style={ loginStyles.buttonContainer}>
+                    <TouchableOpacity
+                        activeOpacity={ 0.8 }
+                        style={ loginStyles.button }
+                        onPress={ onLogin }
+                    >
+                        <Text style={ loginStyles.buttonText}> Login </Text>
+                    </TouchableOpacity>
+                </View>
+
+                {/* Crear una nueva cuenta */}
+                <View style={ loginStyles.newUserContainer}>
+                    <TouchableOpacity
+                        activeOpacity={ 0.8 }
+                        onPress={ () => navigation.replace('RegisterScreen')}
+                    >
+                        <Text style={ loginStyles.buttonText}> Nueva Cuenta </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-
-            {/* Crear una nueva cuenta */}
-            <View style={ loginStyles.newUserContainer}>
-                <TouchableOpacity
-                    activeOpacity={ 0.8 }
-                    onPress={ () => console.log('press')}
-                >
-                    <Text style={ loginStyles.buttonText}> Nueva Cuenta </Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+        </KeyboardAvoidingView>
     </>
   )
 }
