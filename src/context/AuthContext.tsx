b0d5ceb81870e0f4068a044db75e2872003997d8
Paserno <paserno.x@gmail.com>
2022-04-23 22:57:51 -0400

    Manejar errores de login

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -41,15 +41,23 @@ export const AuthProvider = ({ children }: any) => {
                 }
             });
             
-        } catch (err: any) {
-            console.log(err.response.data.msg);
+        } catch (error: any) {
+            console.log(error.response.data.msg);
+            dispatch({
+                type: 'addError',
+                payload: error.response.data.msg || 'Información incorrecta'
+            })
         }
     };
 
 
     const signUp = () => {};
     const logOut = () => {};
-    const removeError = () => {};
+
+    const removeError = () => {
+
+        dispatch({ type: 'removeError'})
+    };
 
     return (
         <AuthContext.Provider value={{
--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,6 +1,13 @@
-import React, { useContext } from 'react'
-import { Text, View, Platform, KeyboardAvoidingView, Keyboard } from 'react-native';
-import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import React, { useContext, useEffect } from 'react'
+import { 
+    Text, 
+    View, 
+    Platform, 
+    KeyboardAvoidingView, 
+    Keyboard, 
+    Alert, 
+    TextInput, 
+    TouchableOpacity } from 'react-native';
 import { Background } from '../components/Background';
 import { WhiteLogo } from '../components/WhiteLogo';
 import { loginStyles } from '../theme/loginTheme';
@@ -13,17 +20,28 @@ interface Props extends StackScreenProps<any, any> {}
 
 export const LoginScreen = ({ navigation }: Props) => {
 
-    const { signIn } = useContext( AuthContext )
+    const { signIn, errorMessage, removeError } = useContext( AuthContext )
 
     const { email, password, onChange } = useForm({
         email: '',
         password: ''
     });
 
+    useEffect(() => {
+      if( errorMessage.length === 0) return;
+
+      Alert.alert('Login incorrecto', errorMessage,[{
+            text: 'Ok',
+            onPress: removeError
+        }]);
+
+    }, [errorMessage])
+    
+
+
     const onLogin = () => {
         console.log({email, password});
         Keyboard.dismiss();
-
         signIn({correo: email, password});
     }
 
